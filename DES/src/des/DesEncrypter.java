18875858b03b2883815e8d27823f2e7dc413ec94
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2018-03-17 23:48:40 -0600

    des cipher

--- DES/src/des/DesEncrypter.java
+++ DES/src/des/DesEncrypter.java
@@ -1,4 +1,5 @@
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -11,101 +12,112 @@ import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.swing.JFileChooser;
 
 class DesEncrypter {
   byte[] buf = new byte[1024];
   Cipher ecipher;
   Cipher dcipher;
   DesEncrypter(SecretKey key) throws Exception{
+    //estos parametros son para cifrados mas avanzados
     byte[] iv = new byte[] { (byte) 0x8E, 0x12, 0x39, (byte) 0x9C, 0x07, 0x72, 0x6F, 0x5A };
     AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
-    ecipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-    dcipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+    
+    ecipher = Cipher.getInstance("DES/ECB/NoPadding");
+    dcipher = Cipher.getInstance("DES/ECB/NoPadding");
 
     ecipher.init(Cipher.ENCRYPT_MODE, key);
     dcipher.init(Cipher.DECRYPT_MODE, key);
   }
 
 
-  public void encrypt(InputStream in, OutputStream out)  throws Exception{
-
-    //leemos y ciframos la imagen
-    out = new CipherOutputStream(out, ecipher);
-    int numRead = 0;   
-    while ((numRead = in.read(buf)) >= 0) {
-      out.write(buf, 0, numRead);
-    }
-    out.close();
-    
-    //leemos y convertimos en un arreglo de bytes la imagen cifrada
-    FileInputStream in2 = new FileInputStream("ciphertext.BMP");
+  public void encrypt(InputStream in, OutputStream out,String path)  throws Exception{
+ 
+    //leemos archivo, creamos un arreglo de bytes sin cabecera
+    in = new FileInputStream(path);
     byte[] buffer = new byte[1024];
     int bytesRead;
     ByteArrayOutputStream output = new ByteArrayOutputStream();
-    while ((bytesRead = in2.read(buffer)) != -1)
+    while ((bytesRead = in.read(buffer)) != -1)
+    {
+        output.write(buffer, 0, bytesRead);
+    }
+    byte[] Decryptedmsg= output.toByteArray();
+    byte[] msg = new byte[Decryptedmsg.length-54];
+    System.arraycopy(Decryptedmsg, 54, msg, 0, msg.length);
+    output.close();
+    
+    //leeemos cabecera
+    FileInputStream in1 = new FileInputStream(path);
+    byte[] header = new byte[54];
+    int numReadb = 0;
+    numReadb = in1.read(header);
+    
+    //escribimos cabecera
+    FileOutputStream out2 = new FileOutputStream("ciphertext.BMP");
+    out2.write(header, 0, header.length);
+    out2.close();
+    
+    //escribimos arreglo cifrado
+    out = new CipherOutputStream(out, ecipher);
+    out.write(msg, 0, msg.length);
+    out.close();  
+
+  }
+
+  public void decrypt(InputStream in, OutputStream out, String path)  throws Exception{   
+    
+    //leemos archivo cifrado, creamos un arreglo de bytes sin cabecera
+    in = new FileInputStream("ciphertext.BMP");
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    while ((bytesRead = in.read(buffer)) != -1)
     {
         output.write(buffer, 0, bytesRead);
     }
     byte[] Encryptedmsg= output.toByteArray();
-    output.close();
+    byte[] msg = new byte[Encryptedmsg.length-54];
+    System.arraycopy(Encryptedmsg, 54, msg, 0, msg.length);
+    output.close(); 
     
-    //leemos la cabecera
-    FileInputStream in3 = new FileInputStream("ASTRO2.BMP");
+    //leeemos cabecera
+    FileInputStream in2 = new FileInputStream(path);
     byte[] header = new byte[54];
     int numReadb = 0;
-    numReadb = in3.read(header);
-      //ponemos la cabecera en el arreglo cifrado
-      System.arraycopy(header, 0, Encryptedmsg, 0, 54);
-        
-    //escribimos la imagen cifrada pero con cabecera sin cifrar
-    FileOutputStream out2 = new FileOutputStream("ciphertext.BMP");
-    out2.write(Encryptedmsg, 0, Encryptedmsg.length);
+    numReadb = in2.read(header);
+    
+    //escribimos cabecera
+    FileOutputStream out2 = new FileOutputStream("cleartext.BMP");
+    out2.write(header, 0, header.length);
     out2.close();
-  }
-
-  public void decrypt(InputStream in, OutputStream out)  throws Exception{
     
-    //deciframos y escribimos 
-    in = new CipherInputStream(in, dcipher);
-    int numRead = 0;
-    while ((numRead = in.read(buf)) >= 0) {
-      out.write(buf, 0, numRead);
-    }
-    out.close();
-    
-    //leemos y convertimos en un arreglo de bytes la imagen cifrada
-    FileInputStream in2 = new FileInputStream("cleartext2.BMP");
-    byte[] buffer = new byte[1024];
-    int bytesRead;
-    ByteArrayOutputStream output = new ByteArrayOutputStream();
-    while ((bytesRead = in2.read(buffer)) != -1)
-    {
-        output.write(buffer, 0, bytesRead);
-    }
-    byte[] DEncryptedmsg= output.toByteArray();    
-    
-    /*//leemos la cabecera
-    FileInputStream in3 = new FileInputStream("ASTRO2.BMP");
-    byte[] header = new byte[54];
-    int numReadb = 0;
-    numReadb = in3.read(header);
-      System.out.println(header.length);
-    //ponemos la cabecera en el arreglo cifrado
-    for(int i = 0; i <54; i++){
-        DEncryptedmsg[i] = header[i];
-    }
-    
-    //escribimos la imagen cifrada pero con cabecera sin cifrar
-    FileOutputStream out2 = new FileOutputStream("cleartext2.BMP");
-    out2.write(DEncryptedmsg, 0, DEncryptedmsg.length);
-    out2.close();*/
+    //deciframos mensaje sin cabecera y escribimos 
+    out = new CipherOutputStream(out, dcipher);
+    out.write(msg, 0, msg.length);
+    out.close(); 
 
   }
   
   public static void main(String[] argv) throws Exception {
     SecretKey key = KeyGenerator.getInstance("DES").generateKey();
     DesEncrypter encrypter = new DesEncrypter(key);
-    encrypter.encrypt(new FileInputStream("ASTRO2.BMP"), new FileOutputStream("ciphertext.BMP"));
-    encrypter.decrypt(new FileInputStream("ciphertext.BMP"), new FileOutputStream("cleartext2.BMP"));
+    
+    JFileChooser jf = new JFileChooser();
+    jf.requestFocus();  /*Lo manda a primer plano*/
+    /*jf.setMultiSelectionEnabled(true) ... (1) */
+    int r = jf.showOpenDialog(null);
+
+    if (r==JFileChooser.APPROVE_OPTION) {
+            /*File[] f = jf.getSelectedFiles()*/
+            File f = jf.getSelectedFile();
+            /*Realizar un for para que vaya leyendo cada uno de los datos del arreglo*/
+            String nombre = f.getName();
+            long tam = f.length(), enviados=0;
+            String path = f.getAbsolutePath();
+    
+    encrypter.encrypt(new FileInputStream(path), new FileOutputStream("ciphertext.BMP",true),path);
+    encrypter.decrypt(new FileInputStream("ciphertext.BMP"), new FileOutputStream("cleartext.BMP",true),path);
+    }
   }
 }
